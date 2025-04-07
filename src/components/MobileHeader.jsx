0b98a268e92f0fc4e7db7dd092e1244f924b36b4
madhan <aptitudealchemy@gmail.com>
2025-04-07 15:20:42 +0530

    Redesigned hero section

--- src/components/MobileHeader.jsx
+++ src/components/MobileHeader.jsx
@@ -33,4 +33,34 @@ function MobileHeader() {
   );
 }
 
+let isToggled = false;
+
+function toggleMenu() {
+  const sidemenu = document.querySelector(".SideMenu");
+  if (!isToggled) {
+    sidemenu.style.display = "block";
+  } else sidemenu.style.display = "none";
+
+  changeToggleIcon();
+  isToggled = !isToggled;
+}
+
+function changeToggleIcon() {
+  const iconElement = document.querySelector(".side-menu-opener");
+  const classes = iconElement.classList;
+
+  if (classes.contains("fa-xmark")) {
+    classes.replace("fa-xmark", "fa-bars");
+  } else if (classes.contains("fa-bars")) {
+    classes.replace("fa-bars", "fa-xmark");
+  }
+}
+
+window.onscroll = (e) => {
+  const sidemenu = document.querySelector(".SideMenu");
+  sidemenu.style.display = "none";
+  if (isToggled) changeToggleIcon();
+  isToggled = false;
+};
+
 export default MobileHeader;
