57965f8d0d4937b5d90b5f31c796e72d4cd94428
madhan <aptitudealchemy@gmail.com>
2025-04-17 10:12:00 +0530

    Setup route...

--- src/components/MobileNav.jsx
+++ src/components/MobileNav.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import SearchLinks from "./SearchLinks";
+import { Link } from "react-router-dom";
 
 function Nav({ navLinks }) {
   return (
@@ -7,9 +8,11 @@ function Nav({ navLinks }) {
       <ul className="p-0 w-100">
         {navLinks.map((link) => (
           <li key={link._id} className="w-100 p-2 bg-transparent mt-2">
-            <a href={`#${link.route}`} className="text-dark">
-              {link.title}
-            </a>
+            <Link
+              to={link.route}
+              title={link.title}
+              className="text-dark"
+            ></Link>
           </li>
         ))}
       </ul>
